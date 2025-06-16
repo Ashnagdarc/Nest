fc694d07ee28672375539b18e85b2971a3973edb
Daniel Samuel <46603289+Ashnagdarc@users.noreply.github.com>
2025-06-16 09:26:52 +0100

    fix: update storageBucket URL in Firebase configuration for correct access to Firestore storage

--- src/hooks/useFCM.ts
+++ src/hooks/useFCM.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDscTln-U_NG5hR0iC1Y0dok_WNTYOZ60E",
     authDomain: "eden-app-notifications.firebaseapp.com",
     projectId: "eden-app-notifications",
-    storageBucket: "eden-app-notifications.appspot.com",
+    storageBucket: "eden-app-notifications.firebasestorage.app",
     messagingSenderId: "456240180306",
     appId: "1:456240180306:web:f3b09f540246c622e316db",
     measurementId: "G-1XDSRDW6BM"
